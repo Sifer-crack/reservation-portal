71c53bb27517fece6f228c68c5efa3edabb87ccb
Sam Andreev <samandreev19@gmail.com>
2025-04-22 08:29:01 +1200

    Backend Initialization

---
+++ reservation-portal-backend/src/main/java/com/samandreev/restaurant/reservation_portal_backend/ReservationPortalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.samandreev.restaurant.reservation_portal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationPortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationPortalBackendApplication.class, args);
+	}
+
+}
---
+++ reservation-portal-backend/src/test/java/com/samandreev/restaurant/reservation_portal_backend/ReservationPortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samandreev.restaurant.reservation_portal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationPortalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
